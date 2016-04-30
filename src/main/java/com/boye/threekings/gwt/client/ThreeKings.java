a0a80644c2026d6ba3cf3725e0120f5d9af0a66e
boy-e <freemna.bo.ye@gmail.com>
2016-04-30 17:53:54 +1200

    separate board & pieces to two layers and change .gitigonre

--- src/main/java/com/boye/threekings/gwt/client/ThreeKings.java
+++ src/main/java/com/boye/threekings/gwt/client/ThreeKings.java
@@ -6,15 +6,26 @@ import static com.boye.threekings.gwt.client.Consts.PIECE_DIAMETER;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ThreeKings implements EntryPoint {
-	private Context2d context;
-
-	private void initBoard() {
+	private Canvas initBoard() {
+		Canvas canvasBoard = Canvas.createIfSupported();
+		canvasBoard.setWidth(BOARD_SIDE + "px");
+		canvasBoard.setCoordinateSpaceWidth(BOARD_SIDE);
+		canvasBoard.setHeight(BOARD_SIDE + "px");
+		canvasBoard.setCoordinateSpaceHeight(BOARD_SIDE);
+		canvasBoard.getElement().getStyle().setPosition(Position.ABSOLUTE);
+		canvasBoard.getElement().getStyle().setLeft(0, Unit.PX);
+		canvasBoard.getElement().getStyle().setTop(0, Unit.PX);
+		canvasBoard.getElement().getStyle().setZIndex(0);
+		Context2d context = canvasBoard.getContext2d();
 		// line 1
 		context.beginPath();
 		context.moveTo(PIECE_DIAMETER / 2, PIECE_DIAMETER / 2);
@@ -41,30 +52,20 @@ public class ThreeKings implements EntryPoint {
 		context.moveTo(BOARD_SIDE - PIECE_DIAMETER / 2, PIECE_DIAMETER / 2);
 		context.lineTo(PIECE_DIAMETER / 2, BOARD_SIDE - PIECE_DIAMETER / 2);
 		context.stroke();
-		// //
-		// context.beginPath();
-		// context.setFillStyle(CssColor.make(255, 0, 0));
-		// // red piece 1
-		// context.arc(PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// // red piece 2
-		// context.arc(BOARD_SIDE / 2, PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// // red piece 3
-		// context.arc(BOARD_SIDE - PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// context.closePath();
-		// context.fill();
-		// context.beginPath();
-		// context.setFillStyle(CssColor.make(0, 255, 0));
-		// // green piece 1
-		// context.arc(PIECE_DIAMETER / 2, BOARD_SIDE - PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// // green piece 2
-		// context.arc(BOARD_SIDE / 2, BOARD_SIDE - PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// // green piece 3
-		// context.arc(BOARD_SIDE - PIECE_DIAMETER / 2, BOARD_SIDE - PIECE_DIAMETER / 2, PIECE_DIAMETER / 2, 0, Math.PI * 2.0, true);
-		// context.closePath();
-		// context.fill();
+		return canvasBoard;
 	}
 
-	private void initPieces() {
+	private Canvas initPieces() {
+		Canvas canvasPieces = Canvas.createIfSupported();
+		canvasPieces.setWidth(BOARD_SIDE + "px");
+		canvasPieces.setCoordinateSpaceWidth(BOARD_SIDE);
+		canvasPieces.setHeight(BOARD_SIDE + "px");
+		canvasPieces.setCoordinateSpaceHeight(BOARD_SIDE);
+		canvasPieces.getElement().getStyle().setPosition(Style.Position.ABSOLUTE);
+		canvasPieces.getElement().getStyle().setLeft(0, Unit.PX);
+		canvasPieces.getElement().getStyle().setTop(0, Unit.PX);
+		canvasPieces.getElement().getStyle().setZIndex(1);
+		Context2d context = canvasPieces.getContext2d();
 		// piece red player
 		PieceComponent pieceRedOne = new PieceComponent(context, PIECE_DIAMETER, Consts.RED, PiecePosition.A1);
 		PieceComponent pieceRedTwo = new PieceComponent(context, PIECE_DIAMETER, Consts.RED, PiecePosition.A2);
@@ -73,29 +74,22 @@ public class ThreeKings implements EntryPoint {
 		PieceComponent pieceGreenOne = new PieceComponent(context, PIECE_DIAMETER, Consts.GREEN, PiecePosition.C1);
 		PieceComponent pieceGreenTwo = new PieceComponent(context, PIECE_DIAMETER, Consts.GREEN, PiecePosition.C2);
 		PieceComponent pieceGreenThree = new PieceComponent(context, PIECE_DIAMETER, Consts.GREEN, PiecePosition.C3);
+		Controller.getInstance().initialize();
 		Controller.getInstance().manage(pieceRedOne);
 		Controller.getInstance().manage(pieceRedTwo);
 		Controller.getInstance().manage(pieceRedThree);
 		Controller.getInstance().manage(pieceGreenOne);
 		Controller.getInstance().manage(pieceGreenTwo);
 		Controller.getInstance().manage(pieceGreenThree);
+		canvasPieces.addMouseDownHandler(new BoardListener());
+		return canvasPieces;
 	}
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		Controller.getInstance().initialize();
-		Canvas canvas = Canvas.createIfSupported();
-		canvas.setWidth(BOARD_SIDE + "px");
-		canvas.setCoordinateSpaceWidth(BOARD_SIDE);
-		canvas.setHeight(BOARD_SIDE + "px");
-		canvas.setCoordinateSpaceHeight(BOARD_SIDE);
-		RootPanel.get("ThreeKingsContainer").add(canvas);
-		//
-		context = canvas.getContext2d();
-		initBoard();
-		initPieces();
-		canvas.addMouseDownHandler(new BoardListener());
+		RootPanel.get("ThreeKingsContainer").add(initBoard());
+		RootPanel.get("ThreeKingsContainer").add(initPieces());
 	}
 }
